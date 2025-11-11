40ed00d68f446d9ba242e6f37437a2e5ca80fed5
Eda Nehir Gül <edaade278@gmail.com>
2025-11-11 21:54:36 +0300

    f

--- 16.10.2025-4.c
+++ 16.10.2025-4.c
@@ -1,21 +1,23 @@
 #include <math.h>
 #include <stdio.h>
 
-float UcgenAlan(float x, float y);
+float UcgenAlan(float x, float y) {
 float result =( x*y/2);
 return result; // fonksiyonlarda result tanımladıktan sonra return result
-
-
+}
 
 int main()
-{
+{ 
     float x , y ;
-    printf("Uçgenin yüksekliği: \n");
+    printf("Ucgenin yüksekliği: \n");
     scanf("%f", &x);
 
     printf("Ucgenin tabanı: \n");
     scanf("%f", &y);
 
-    float alan= 
+    float alan;
 
-}
+    printf("Üçgenin Alanı: %f\n" , alan);
+    return 0;
+
+} 
